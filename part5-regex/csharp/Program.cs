5d95c1455b0bc5dcbbe6b8443ba35dca7b510f6d
user-nexus <henrique.p.lima0@gmail.com>
2022-02-21 18:11:29 -0300

    Sending part 5 files

---
+++ part5-regex/csharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            string alvo = "11a22b33c";
+            string pattern = @"(\d\d)(\w)";
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.matches(alvo);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Groups[0].value);
+                Console.WriteLine(match.Groups[1].value);
+                Console.WriteLine(match.Groups[2].value);
+                Console.WriteLine(match.Index);
+                Console.WriteLine(match.Length);
+            }
+        }
+    }
+}
